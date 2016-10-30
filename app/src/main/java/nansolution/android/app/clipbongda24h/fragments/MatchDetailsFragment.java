d8f8d2596265b097a24d31e603a544d3e3e2745a
phandinhthai <thai@planday.com>
2016-10-30 15:00:27 +0700

    add shit for VungLe Ads

--- app/src/main/java/nansolution/android/app/clipbongda24h/fragments/MatchDetailsFragment.java
+++ app/src/main/java/nansolution/android/app/clipbongda24h/fragments/MatchDetailsFragment.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -124,7 +123,7 @@ public class MatchDetailsFragment extends Fragment implements View.OnClickListen
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.layout_match_detail, container, false);
         videoView = (VideoView) view.findViewById(R.id.main_video);
         mActivity = (MainActivity) getActivity();
         mProgressDialog = new ProgressDialog(getContext());
